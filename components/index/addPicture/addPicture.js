103ecdd3aa20f987ca7a467429422dea124deb52
17671448657 <416317444@qq.com>
2020-11-11 15:15:08 +0800

    图片视频上传

--- components/index/addPicture/addPicture.js
+++ components/index/addPicture/addPicture.js
@@ -2,14 +2,54 @@ Component({
   options: {
     addGlobalClass: true
   },
-  data:{
-    isClose:true
+  data: {
+    isClose: true
   },
-  methods:{
-    switchPopUp(){
+  methods: {
+    switchPopUp() {
       this.setData({
         isClose: !this.data.isClose
       })
     },
+    chooseImage() {
+      this.switchPopUp()
+      setTimeout(() => {
+        wx.chooseImage({
+          success(res) {
+            console.log(res);
+          }
+        })
+      }, 200)
+    },
+    chooseVideo() {
+      this.switchPopUp()
+      wx.chooseVideo({
+        success: (result) => {
+          console.log(result);
+        },
+      })
+    },
+    chooseMessageImage() {
+      this.switchPopUp()
+      setTimeout(() => {
+        wx.chooseMessageFile({
+          type: "image",
+          success(res) {
+            console.log(res);
+          }
+        })
+      }, 200)
+    },
+    chooseMessageVideo() {
+      this.switchPopUp()
+      setTimeout(() => {
+        wx.chooseMessageFile({
+          type: "video",
+          success(res) {
+            console.log(res);
+          }
+        })
+      }, 200)
+    }
   }
 })
